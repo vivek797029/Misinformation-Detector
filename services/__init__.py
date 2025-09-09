420f8bf5ec0c704c13f4f1c67f30d3df4ce93f89
Hermes9900 <avikkivaanime@gmail.com>
2025-09-09 07:32:02 +0530

    Delete services/__init__.py

--- services/__init__.py
+++
@@ -1,9 +0,0 @@
-# This file initializes the 'services' directory as a Python package.
-
-# By importing the main functions from our modules here, we can make them
-# directly accessible from the 'services' package itself. This simplifies
-# imports in other parts of our application, like app.py.
-
-from .factcheck import fact_check
-from .media_check import check_image
-from .score import credibility_score
